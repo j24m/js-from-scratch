7ed9f2e79d4aeaf7f91e58d683d60bf701d67760
Jyotsna Mehta <53934353+j24m@users.noreply.github.com>
2022-03-05 21:03:53 +0530

    Values and Variables in js.

--- starter/script.js
+++ starter/script.js
@@ -1,3 +1,40 @@
+////////////////////////////////
+// Linking js file
+
 let js = 'amazing';
 if (js === 'amazing') alert('js is fun');
 console.log(40 + 8 + 23 - 10);
+
+////////////////////////////////
+
+////////////////////////////////
+// Values and Vairables
+
+console.log("Jonas");
+console.log(23);
+
+let firstName= "Matilda";
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+// correct naming conventions for variables
+// let first ='jonas';
+// let firstNamePerson;  camelcase is the standard
+// let first_name_person;
+
+// incorrect naming conventions
+// let 3years;
+// let jonas&matilda ='JM'; & cannot be used in var names.
+// let function = "hello"; reserved keyword
+// let new = "27"; reserved keyword
+
+// not recommended to use capital letter at first word.
+let Sum = "hello";
+let sum = hello;
+console.log(Sum);
+
+let myFirstJob ="programmer";
+let myCurrentJob = "teacher";
+
+////////////////////////////////
