ab66ca4db7e2aad9e4593e8da85b8feb843c4a86
Jyotsna Mehta <53934353+j24m@users.noreply.github.com>
2022-03-06 15:58:30 +0530

    Did datatypes and their assignment.

--- starter/assignments.js
+++ starter/assignments.js
@@ -1,11 +1,31 @@
+            // ASSIGNMENTS
+
 ///////////////////////////////
 // Variables and Values
 
 var country ="India";
 console.log(country);
+console.log(typeof country);
 
 var continent = "Asia";
 console.log(continent);
+console.log(typeof continent);
+
 
 var population = 1.366 + " billion";
 console.log(population);
+console.log(typeof population);
+
+///////////////////////////////
+
+
+///////////////////////////////
+//Datatypes
+
+let isIsland = 'Asia';
+console.log(isIsland);
+console.log(typeof isIsland);
+let language;
+console.log(language);
+
+///////////////////////////////
--- starter/script.js
+++ starter/script.js
@@ -1,22 +1,22 @@
 ////////////////////////////////
 // Linking js file
 
-let js = 'amazing';
-if (js === 'amazing') alert('js is fun');
-console.log(40 + 8 + 23 - 10);
+// let js = 'amazing';
+// if (js === 'amazing') alert('js is fun');
+// console.log(40 + 8 + 23 - 10);
 
 ////////////////////////////////
 
 ////////////////////////////////
 // Values and Vairables
 
-console.log("Jonas");
-console.log(23);
+// console.log("Jonas");
+// console.log(23);
 
-let firstName= "Matilda";
-console.log(firstName);
-console.log(firstName);
-console.log(firstName);
+// let firstName= "Matilda";
+// console.log(firstName);
+// console.log(firstName);
+// console.log(firstName);
 
 // correct naming conventions for variables
 // let first ='jonas';
@@ -30,11 +30,44 @@ console.log(firstName);
 // let new = "27"; reserved keyword
 
 // not recommended to use capital letter at first word.
-let Sum = "hello";
-let sum = "hello";
-console.log(sum);
+// let Sum = "hello";
+// let sum = "hello";
+// console.log(sum);
 
-let myFirstJob ="programmer";
-let myCurrentJob = "teacher";
+// let myFirstJob ="programmer";
+// let myCurrentJob = "teacher";
+
+////////////////////////////////
+
+
+////////////////////////////////
+// Datatypes
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof true); 
+console.log(typeof javascriptIsFun); 
+console.log(typeof 23); 
+console.log(typeof "hello");
+
+// Dynamic typing.
+javascriptIsFun = 'YES!';
+console.log(javascriptIsFun);
+console.log(typeof javascriptIsFun); 
+
+// undefined.
+let year;
+console.log(year);  
+console.log(typeof year);
+// Whenever we declare an empty variable both value and type of that variable are undefined.
+// We can also reassign a value to undefined variables.
+
+year = 1991;
+console.log(year);
+console.log(typeof year);
+
+console.log(typeof null);
+// This is a bug in javascript, the output should not be object, but null.
 
 ////////////////////////////////
