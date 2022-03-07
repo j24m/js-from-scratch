9e4bcd59271a0ab38bde549908209266ae6fc6c3
Jyotsna Mehta <53934353+j24m@users.noreply.github.com>
2022-03-08 01:06:32 +0530

    did type conversion and type coersion and their assignment.

--- starter/script.js
+++ starter/script.js
@@ -181,3 +181,27 @@
 //     console.log(`You are not eligible to vote and you will be eligible after ${yearsLeft} year.`);
 // } 
 ///////////////////////////////
+
+///////////////////////////////
+// Type Conversion and Coercion
+
+// type conversion
+// const inputYear = '1991';
+// console.log(Number(inputYear), inputYear);
+// console.log(Number(inputYear) + 18);
+
+// console.log(Number('Jonas'));
+// console.log(typeof NaN);
+
+// console.log(String(23), 23);
+
+// // type coercion
+// console.log('I am ' + 23 + ' years old');
+// console.log('23' - '10' - 3);
+// console.log('23' / '2');
+
+// let n = '1' + 1; // '11'
+// n = n - 1;
+// console.log(n);
+
+///////////////////////////////
