7facfbbaf0e138d9ff11aea127eb3bae86e310b2
Jyotsna Mehta <53934353+j24m@users.noreply.github.com>
2022-03-08 01:16:44 +0530

    did truthy and falsy values.

--- starter/script.js
+++ starter/script.js
@@ -205,3 +205,25 @@
 // console.log(n);
 
 ///////////////////////////////
+
+
+///////////////////////////////
+//Truthy and falsy values
+
+console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean('Jonas'));
+console.log(Boolean({}));
+console.log(Boolean(''));
+console.log(Boolean(null));
+console.log(Boolean(NaN));
+
+let money = 100;
+if(money) console.log(`Don't spend it all.`)
+else console.log(`You should get a job.`);
+
+let height;
+if(height) console.log("height is defined.");
+else console.log("height is undefined.");
+
+///////////////////////////////
